8b0146bd295c23d571b86e196401bc967afb1304
rossorapper-rgb <rossorapper@gmail.com>
2026-09-17 22:55:53 +0000

    fix: retour Paramètres vers le dashboard Bergerie

--- lib/features/parametres/pages/parametres_bergerie_page.dart
+++ lib/features/parametres/pages/parametres_bergerie_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ParametresBergeriePage extends StatelessWidget {
   const ParametresBergeriePage({super.key});
@@ -10,7 +11,7 @@ class ParametresBergeriePage extends StatelessWidget {
         title: const Text('Paramètres'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
+          onPressed: () => context.go('/dashboard/bergerie'),
         ),
       ),
       body: ListView(
