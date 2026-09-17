5edcdbed74a3081f7acd5bf0c5e356764df77382
rossorapper-rgb <rossorapper@gmail.com>
2026-09-17 22:48:09 +0000

    feat: ajouter la route Paramètres Bergerie

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -19,6 +19,7 @@ import '../features/alimentation/pages/alimentation_page.dart';
 import '../features/finances/pages/finances_bergerie_page.dart';
 import '../features/naissances/pages/naissances_page.dart';
 import '../features/rapports/pages/rapports_bergerie_page.dart';
+import '../features/parametres/pages/parametres_bergerie_page.dart';
 import '../features/abonnements/pages/abonnements_page.dart';
 import '../features/finances/pages/finances_home_page.dart';
 import '../features/finances/pages/paiements_page.dart';
@@ -124,6 +125,7 @@ final GoRouter appRouter = GoRouter(
     GoRoute(path: '/gestations', redirect: (context, state) => _gestationRedirect(), builder: (context, state) => const GestationsPage()),
     GoRoute(path: '/naissances', redirect: (context, state) => _naissanceRedirect(), builder: (context, state) => const NaissancesPage()),
     GoRoute(path: '/rapports', redirect: (context, state) => _rapportBergerieRedirect(), builder: (context, state) => const RapportsBergeriePage()),
+    GoRoute(path: '/parametres', redirect: (context, state) => _financeBergerieRedirect(), builder: (context, state) => const ParametresBergeriePage()),
     GoRoute(path: '/allo-veto', builder: (context, state) => const AlloVetoPage()),
     GoRoute(path: '/carnet-sante', redirect: (context, state) => _santeRedirect(), builder: (context, state) => const CarnetSantePage()),
     GoRoute(path: '/alimentation', redirect: (context, state) => _alimentationRedirect(), builder: (context, state) => const AlimentationPage()),
