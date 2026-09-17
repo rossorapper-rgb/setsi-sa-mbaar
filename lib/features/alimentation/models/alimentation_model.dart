234becb2bb68fe86688eda722c900a481a5e1616
rossorapper-rgb <rossorapper@gmail.com>
2026-09-17 16:07:50 +0000

    feat: ajouter le modèle Alimentation simple

---
+++ lib/features/alimentation/models/alimentation_model.dart
@@ -0,0 +1,73 @@
+class AlimentationModel {
+  final String id;
+  final String bergerieId;
+  final String aliment;
+  final double quantite;
+  final String unite;
+  final double prix;
+  final DateTime date;
+  final String observation;
+
+  const AlimentationModel({
+    required this.id,
+    required this.bergerieId,
+    required this.aliment,
+    required this.quantite,
+    required this.unite,
+    required this.prix,
+    required this.date,
+    this.observation = '',
+  });
+
+  AlimentationModel copyWith({
+    String? id,
+    String? bergerieId,
+    String? aliment,
+    double? quantite,
+    String? unite,
+    double? prix,
+    DateTime? date,
+    String? observation,
+  }) {
+    return AlimentationModel(
+      id: id ?? this.id,
+      bergerieId: bergerieId ?? this.bergerieId,
+      aliment: aliment ?? this.aliment,
+      quantite: quantite ?? this.quantite,
+      unite: unite ?? this.unite,
+      prix: prix ?? this.prix,
+      date: date ?? this.date,
+      observation: observation ?? this.observation,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'bergerieId': bergerieId,
+      'aliment': aliment,
+      'quantite': quantite,
+      'unite': unite,
+      'prix': prix,
+      'date': date.millisecondsSinceEpoch,
+      'observation': observation,
+    };
+  }
+
+  factory AlimentationModel.fromMap(Map<String, dynamic> map) {
+    final rawDate = map['date'];
+    final date = rawDate is int
+        ? DateTime.fromMillisecondsSinceEpoch(rawDate)
+        : DateTime.now();
+
+    return AlimentationModel(
+      id: map['id']?.toString() ?? '',
+      bergerieId: map['bergerieId']?.toString() ?? '',
+      aliment: map['aliment']?.toString() ?? '',
+      quantite: (map['quantite'] as num?)?.toDouble() ?? 0,
+      unite: map['unite']?.toString() ?? 'kg',
+      prix: (map['prix'] as num?)?.toDouble() ?? 0,
+      date: date,
+      observation: map['observation']?.toString() ?? '',
+    );
+  }
+}
