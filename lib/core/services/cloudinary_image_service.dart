ed54f4ad4d7d6e70abab95127feb8bdd81960fbb
rossorapper-rgb <rossorapper@gmail.com>
2026-09-17 17:55:54 +0000

    feat: ajouter upload Cloudinary des photos de naissance

--- lib/core/services/cloudinary_image_service.dart
+++ lib/core/services/cloudinary_image_service.dart
@@ -8,15 +8,14 @@ import '../config/cloudinary_config.dart';
 class CloudinaryImageService {
   const CloudinaryImageService();
 
-  Future<String> uploadMoutonPhoto({
+  Future<String> _upload({
     required Uint8List bytes,
-    required String bergerieId,
-    required String moutonId,
+    required String folder,
+    required String publicId,
+    required String filename,
   }) async {
     if (!CloudinaryConfig.isConfigured) {
-      throw Exception(
-        'Cloudinary n’est pas encore configuré. Renseignez le cloud name et le upload preset.',
-      );
+      throw Exception('Cloudinary n’est pas encore configuré.');
     }
 
     final uri = Uri.parse(
@@ -25,15 +24,11 @@ class CloudinaryImageService {
 
     final request = http.MultipartRequest('POST', uri)
       ..fields['upload_preset'] = CloudinaryConfig.uploadPreset
-      ..fields['folder'] = 'bergeries/$bergerieId/moutons'
-      ..fields['public_id'] = 'mouton_$moutonId';
+      ..fields['folder'] = folder
+      ..fields['public_id'] = publicId;
 
     request.files.add(
-      http.MultipartFile.fromBytes(
-        'file',
-        bytes,
-        filename: 'mouton_$moutonId.jpg',
-      ),
+      http.MultipartFile.fromBytes('file', bytes, filename: filename),
     );
 
     final response = await request.send();
@@ -41,27 +36,47 @@ class CloudinaryImageService {
 
     if (response.statusCode < 200 || response.statusCode >= 300) {
       String message = 'Échec de l’envoi de la photo.';
-
       try {
         final data = jsonDecode(responseBody);
         final error = data['error'];
         if (error is Map && error['message'] != null) {
           message = error['message'].toString();
         }
-      } catch (_) {
-        // On conserve le message générique si la réponse n'est pas JSON.
-      }
-
+      } catch (_) {}
       throw Exception(message);
     }
 
     final data = jsonDecode(responseBody) as Map<String, dynamic>;
     final secureUrl = data['secure_url']?.toString() ?? '';
-
     if (secureUrl.isEmpty) {
       throw Exception('Cloudinary n’a pas retourné l’URL de la photo.');
     }
-
     return secureUrl;
+  }
+
+  Future<String> uploadMoutonPhoto({
+    required Uint8List bytes,
+    required String bergerieId,
+    required String moutonId,
+  }) {
+    return _upload(
+      bytes: bytes,
+      folder: 'bergeries/$bergerieId/moutons',
+      publicId: 'mouton_$moutonId',
+      filename: 'mouton_$moutonId.jpg',
+    );
+  }
+
+  Future<String> uploadNaissancePhoto({
+    required Uint8List bytes,
+    required String bergerieId,
+    required String naissanceId,
+  }) {
+    return _upload(
+      bytes: bytes,
+      folder: 'bergeries/$bergerieId/naissances',
+      publicId: 'naissance_$naissanceId',
+      filename: 'naissance_$naissanceId.jpg',
+    );
   }
 }
