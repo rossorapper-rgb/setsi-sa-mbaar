d6b337dcec860071e251d032e2a24e64d62cd46c
rossorapper-rgb <rossorapper@gmail.com>
2026-09-18 23:07:02 +0000

    fix: guard browser back forward bfcache after logout

--- lib/core/navigation/browser_history_guard_web.dart
+++ lib/core/navigation/browser_history_guard_web.dart
@@ -8,9 +8,20 @@ StreamSubscription<html.PopStateEvent>? _subscription;
 void installBrowserHistoryGuard() {
   _subscription?.cancel();
   _subscription = html.window.onPopState.listen((_) {
-    if (FirebaseAuth.instance.currentUser == null &&
-        html.window.location.pathname != '/login') {
-      html.window.location.replace('/login');
-    }
+    _redirectIfLoggedOut();
   });
+
+  html.window.onPageShow.listen((_) {
+    // Chrome can restore a previous Flutter Web document from bfcache
+    // when the user presses Back/Forward. In that case popstate alone
+    // is not enough to re-run the authentication guard.
+    _redirectIfLoggedOut();
+  });
+}
+
+void _redirectIfLoggedOut() {
+  if (FirebaseAuth.instance.currentUser == null &&
+      html.window.location.pathname != '/login') {
+    html.window.location.replace('/login');
+  }
 }
