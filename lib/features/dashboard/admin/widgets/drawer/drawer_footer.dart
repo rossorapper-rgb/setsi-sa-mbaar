ddcdf9eb2a7b8857ae6fc33add90d197f80b05c5
rossorapper-rgb <rossorapper@gmail.com>
2026-09-19 11:00:55 +0000

    fix: corriger les imports du footer administrateur

--- lib/features/dashboard/admin/widgets/drawer/drawer_footer.dart
+++ lib/features/dashboard/admin/widgets/drawer/drawer_footer.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../../../core/config/current_bergerie_config.dart';
-import '../../../../core/session/current_user_service.dart';
+import '../../../../../core/config/current_bergerie_config.dart';
+import '../../../../../core/session/current_user_service.dart';
 
 class DrawerFooter extends StatelessWidget {
   const DrawerFooter({
