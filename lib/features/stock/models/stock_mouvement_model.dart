ffcc18f4a874222db249293ca7cbfa07d8536f36
rossorapper-rgb <rossorapper@gmail.com>
2026-09-25 16:32:00 +0000

    feat(stock): ajouter la lecture des mouvements

--- lib/features/stock/models/stock_mouvement_model.dart
+++ lib/features/stock/models/stock_mouvement_model.dart
@@ -25,6 +25,31 @@ class StockMouvementModel {
     this.note = '',
   });
 
+  factory StockMouvementModel.fromMap(Map<String, dynamic> map) {
+    final rawDate = map['date'];
+    DateTime date;
+    if (rawDate is Timestamp) {
+      date = rawDate.toDate();
+    } else if (rawDate is DateTime) {
+      date = rawDate;
+    } else {
+      date = DateTime.tryParse(rawDate?.toString() ?? '') ?? DateTime.now();
+    }
+
+    return StockMouvementModel(
+      id: map['id']?.toString() ?? '',
+      bergerieId: map['bergerieId']?.toString() ?? '',
+      produitId: map['produitId']?.toString() ?? '',
+      type: map['type']?.toString() ?? '',
+      quantite: (map['quantite'] as num?)?.toDouble() ?? 0,
+      date: date,
+      motif: map['motif']?.toString() ?? '',
+      prix: (map['prix'] as num?)?.toDouble() ?? 0,
+      fournisseur: map['fournisseur']?.toString() ?? '',
+      note: map['note']?.toString() ?? '',
+    );
+  }
+
   Map<String, dynamic> toMap() => {
         'bergerieId': bergerieId,
         'produitId': produitId,
